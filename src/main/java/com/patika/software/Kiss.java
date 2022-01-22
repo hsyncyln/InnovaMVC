61511c35736e316538668d7689a77aaf365862d3
huseyin.ceylan <huseyinceylantr@gmail.com>
2022-01-22 18:05:32 +0300

    Kiss - File Reader, File Writer

---
+++ src/main/java/com/patika/software/Kiss.java
@@ -0,0 +1,57 @@
+package com.patika.software;
+
+import lombok.extern.log4j.Log4j2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import javax.swing.*;
+
+//Keep It Simple Stupid : Olabildiğince basit
+@Log4j2
+public class Kiss {
+
+    public String path = "C:\\patika\\patika.txt";
+
+    //File Writer
+    public void utilWriter(){
+        //try with resources
+        try(BufferedWriter bufferredWriter =
+                    new BufferedWriter(new FileWriter(path,true))) {
+
+            String value = JOptionPane.showInputDialog("Lütfen bir şeyler giriniz");
+            bufferredWriter.write(value);
+            bufferredWriter.flush();
+
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+
+    //File Reader
+    public void utilReader() {
+        //try with resources
+        try(BufferedReader bufferedReader =
+                    new BufferedReader(new FileReader(path))) {
+
+            String numberRow = "", sumRow = "\n";
+
+            while ((numberRow = bufferedReader.readLine()) != null){
+                sumRow += numberRow + "\n";
+            }
+            log.info(sumRow);
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+         Kiss kiss = new Kiss();
+         kiss.utilReader();
+    }
+
+}
