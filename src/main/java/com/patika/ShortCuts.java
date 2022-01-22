efa4cf54d3f96731e8edc03e0ee0fe0581d792a3
huseyin.ceylan <huseyinceylantr@gmail.com>
2022-01-22 18:06:04 +0300

    ShortCuts

---
+++ src/main/java/com/patika/ShortCuts.java
@@ -0,0 +1,17 @@
+package com.patika;
+
+public class ShortCuts {
+
+  //  IntelliJ IDEA Shortcuts
+    // Settings -> Editor -> Code Style -> Inspections -> Proofreading -> Typo (Yazım hatalarının altını çizerek belirtme/belirtmeme)
+    //Alt + Insert --> Getter, Setter, toString etc. Kısa Yol Menüsü
+    //Ctrl + Shift + S --> Power Save Mode
+    //Shift + F6 --> Refactor (Rename)
+    //Ctrl + Shift + F10 --> Run Project
+    //Ctrl + Shift + F9 --> Build Project
+    //Ctrl + Shift + F2 --> Terminate (Stop) Project
+    //Ctrl + Shift + O --> Maven Install
+    //Ctrl + Shift + K --> Git Push
+    //Shift + K --> Git Commit
+    //Ctrl + Shift + (-) --> collapse
+}
